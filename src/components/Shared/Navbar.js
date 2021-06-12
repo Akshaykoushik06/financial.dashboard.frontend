afe00809b35df908aa45e6ecf27a441b70e3cd55
Akshaykoushik06 <akshaykoushikhothur@gmail.com>
2021-06-12 13:47:51 +0530

    fixed console warnings for anchor tags

--- src/components/Shared/Navbar.js
+++ src/components/Shared/Navbar.js
@@ -9,53 +9,64 @@ function Navbar() {
         <nav className="navbar navbar-expand-md sticky-top navbar-dark bg-dark">
             <div className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
                 <ul className="navbar-nav mr-auto">
-                    <Link to='/stocks'>
+                    <Link to="/stocks">
                         <li className="nav-item active">
-                            <a className="nav-link">Stocks</a>
+                            <span className="nav-link">Stocks</span>
                         </li>
                     </Link>
-                    <Link to='/mutualfunds'>
+                    <Link to="/mutualfunds">
                         <li className="nav-item">
-                            <a className="nav-link">Mutual Funds</a>
+                            <span className="nav-link">Mutual Funds</span>
                         </li>
                     </Link>
-                    <Link to='/gold'>
+                    <Link to="/gold">
                         <li className="nav-item">
-                            <a className="nav-link">Gold</a>
+                            <span className="nav-link">Gold</span>
                         </li>
                     </Link>
-                    <Link to='/foodcard'>
+                    <Link to="/foodcard">
                         <li className="nav-item">
-                            <a className="nav-link">Food Card</a>
+                            <span className="nav-link">Food Card</span>
                         </li>
                     </Link>
-                    <Link to='/pf'>
+                    <Link to="/pf">
                         <li className="nav-item">
-                            <a className="nav-link">Provident Fund</a>
+                            <span className="nav-link">Provident Fund</span>
                         </li>
                     </Link>
                 </ul>
             </div>
             <div className="mx-auto order-0">
-                <Link to='/'>
-                    <a className="navbar-brand mx-auto" href="#">Financial Dashboard</a>
+                <Link to="/">
+                    <span className="navbar-brand mx-auto">
+                        Financial Dashboard
+                    </span>
                 </Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target=".dual-collapse2"
+                >
                     <span className="navbar-toggler-icon"></span>
                 </button>
             </div>
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
                 <ul className="navbar-nav ml-auto">
                     <li className="nav-item">
-                        <a className="nav-link" href="#"><ProfileIcon /></a>
+                        <span className="nav-link">
+                            <ProfileIcon />
+                        </span>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#"><SettingsIcon /></a>
+                        <span className="nav-link">
+                            <SettingsIcon />
+                        </span>
                     </li>
                 </ul>
             </div>
         </nav>
-    )
+    );
 }
 
 export default Navbar;
